cedd55164a27a7adba1987302041ea4e4964c144
carrvo <carrvo@gmail.com>
2022-05-04 20:44:10 -0600

    fix tests import

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ from test.interest_tests import InterestTests
 from test.timevalue_tests import TimeValueTests
 from test.examples import TimeValueLibExamples
 
-__all__ = ['InterestTests', 'TimeValueTests']
+__all__ = ['InterestTests', 'TimeValueTests', 'TimeValueLibExamples']
