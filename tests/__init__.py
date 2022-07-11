fc1ff57fde02677b4dddd3c826bc1a54149b7093
carrvo <carrvo@gmail.com>
2022-07-10 21:01:57 -0600

    fix unittest
    allows for the use of
    `py -3 -m tests`
    over the use of
    `py -3 tests`

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 
-import test
-from test.interest_tests import InterestTests
-from test.timevalue_tests import TimeValueTests
-from test.examples import TimeValueLibExamples
+import tests
+from tests.interest_tests import InterestTests
+from tests.timevalue_tests import TimeValueTests
+from tests.examples import TimeValueLibExamples
 
 __all__ = ['InterestTests', 'TimeValueTests', 'TimeValueLibExamples']
--- tests/__main__.py
+++ tests/__main__.py
@@ -1,12 +1,5 @@
 import unittest
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.curdir))
-
-import test
-from interest_tests import InterestTests
-from timevalue_tests import TimeValueTests
-from examples import TimeValueLibExamples
+from tests import *
 
 unittest.main()
