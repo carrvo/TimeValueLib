eba64473377af3274bec43da4c335062dc4a6040
carrvo <carrvo@gmail.com>
2022-07-11 20:50:05 -0600

    added docstrings to dateserializer

--- timevaluelib/dateserializer.py
+++ timevaluelib/dateserializer.py
@@ -1,9 +1,19 @@
+"""
+Date manipulation functions.
+"""
 
 import datetime
 
 import timevaluelib
 
 def parseDate(date, format=None):
+    """
+    Parses a date from a string or copies a datetime.date object.
+
+    @Params:
+        - date: to be parsed
+        - format: the string format the date parameter is represented as
+    """
     if isinstance(date, datetime.date):
         return datetime.date(date.year, date.month, date.day)
     elif format:
@@ -14,6 +24,13 @@ def parseDate(date, format=None):
         return datetime.date.fromisoformat(date)
 
 def formatDate(date, format=None):
+    """
+    Formats a date into a string.
+
+    @Params:
+        - date: to be formatted
+        - format: the string format the date parameter will be represented as
+    """
     if format:
         return date.strftime(format)
     elif timevaluelib.DATE_FORMAT:
